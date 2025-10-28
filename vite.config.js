517bad77573de9ec9275b1e12e991cf440ed1f61
ranjan-builds <ranjankashyap6475@gmail.com>
2025-10-28 07:59:21 +0530

    fix

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import path from "path"
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/ranjanflix/',
+  base: '/RanjanFlix/',
   plugins: [react()],
   resolve: {
     alias: {
